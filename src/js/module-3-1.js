e82f186378d1e531eaa1dabce56c41a89ed8fdff
Diana Muzhylivska <diana.listen@gmail.com>
2026-09-01 14:52:14 +0300

    add in module-3-1.js solution of Kata 10

--- src/js/module-3-1.js
+++ src/js/module-3-1.js
@@ -737,6 +737,23 @@ function myReverse(arr) {
 }
 
 // - [Kata #10](https://www.codewars.com/kata/58f8a3a27a5c28d92e000144)
+// Your task is to find the first element of an array that is not consecutive.
+
+// By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+// E.g. If we have an array [1,2,3,4,6,7,8] then 1 then 2 then 3 then 4 are all consecutive but 6 is not, so that's the first non-consecutive number.
+
+// If the whole array is consecutive then return null.
+
+// The array will always have at least 2 elements1 and all elements will be numbers. The numbers will also all be unique and in ascending order. The numbers could be positive or negative and the first non-consecutive could be either too!
+function areAllConsecutive(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] - arr[i] !== 1) {
+      return arr[i + 1];
+    }
+    return null;
+  }
+}
 // - [Kata #11](https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118)
 // - [Kata #12](https://www.codewars.com/kata/5a905c2157c562994900009d)
 // - [Kata #13](https://www.codewars.com/kata/57d814e4950d8489720008db)
