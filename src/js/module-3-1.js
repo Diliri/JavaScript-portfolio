cc695c87963ff9b35575332565e73814f1472710
Diana Muzhylivska <diana.listen@gmail.com>
2026-09-02 23:59:54 +0300

    h

--- src/js/module-3-1.js
+++ src/js/module-3-1.js
@@ -755,6 +755,9 @@ function areAllConsecutive(arr) {
   }
 }
 // - [Kata #11](https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118)
+function distinct(a) {
+  return [...new Set(a)];
+}
 // - [Kata #12](https://www.codewars.com/kata/5a905c2157c562994900009d)
 // - [Kata #13](https://www.codewars.com/kata/57d814e4950d8489720008db)
 // - [Kata #14](https://www.codewars.com/kata/5a2be17aee1aaefe2a000151)
